cf1dfd2de25682047b15ee9557e2503fdb6846c4
mfarhan08a <farhanputra.farhan8@gmail.com>
2023-05-26 23:44:12 +0700

    init, create xml ui

---
+++ app/src/main/java/com/mfarhan08a/hangoutyuk/ui/chats/ChatsFragment.kt
@@ -0,0 +1,38 @@
+package com.mfarhan08a.hangoutyuk.ui.chats
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.mfarhan08a.hangoutyuk.databinding.FragmentChatsBinding
+
+class ChatsFragment : Fragment() {
+
+    private var _binding: FragmentChatsBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val chatsViewModel =
+            ViewModelProvider(this)[ChatsViewModel::class.java]
+
+        _binding = FragmentChatsBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/mfarhan08a/hangoutyuk/ui/detail/DetailActivity.kt
@@ -0,0 +1,12 @@
+package com.mfarhan08a.hangoutyuk.ui.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mfarhan08a.hangoutyuk.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
